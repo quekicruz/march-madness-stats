dbd20b052dab37bd0c314480e9f23e9e98df8624
Quentin Cruz <napu0515@gmail.com>
2021-03-24 17:05:53 -0700

    marchmadness

---
+++ mm.js
@@ -0,0 +1,108 @@
+let userName = prompt('Who is your Sweet 16 Team?');
+
+
+if (userName == 'Duke'){
+    document.write('<h3>' + 'Welcome Blue Devil' + '</h3>');
+} else 
+{
+    document.write('<h4>' + 'Welcome' + '</h4>');
+}
+
+if (userName == 'Gonzga'){
+    document.write('<h3>' + 'Welcome Bulldog' + '</h3>');
+}else 
+{
+        document.write('<h4>' + 'Welcome' + '</h4>');
+    }
+
+if (userName == 'Creighton'){
+    document.write('<h3>' + 'Welcome BlueJay ' + '</h3>');
+} else {
+    document.write('<h4>' + 'Welcome' + '</h4>');
+}
+
+if (userName == 'USC'){
+    document.write('<h3>' + 'Welcome Traveler ' + '</h3>');
+    
+} else {
+        document.write('<h4>' + 'Welcome' + '</h4>');
+    }
+
+if (userName == 'Oregon'){
+    document.write('<h3>' + 'Welcome Duck' + '</h3>');
+} else {
+    document.write('<h4>' + 'Welcome' + '</h4>');
+}
+
+if (userName == 'Michigan'){
+    document.write('<h3>' + 'Welcome Wolverine' + '</h3>');
+} else {
+    document.write('<h4>' + 'Welcome' + '</h4>');
+}
+
+if (userName == 'Florida State'){
+    document.write('<h3>' + 'Welcome Renegade' + '</h3>');
+} else {
+    document.write('<h4>' + 'Welcome' + '</h4>');
+}
+
+if (userName == 'UCLA'){
+    document.write('<h3>' + 'Welcome Bruin' + '</h3>');
+} else {
+    document.write('<h4>' + 'Welcome' + '</h4>');
+}
+ 
+if (userName == 'Alabama'){
+    document.write('<h3>' + 'Welcome Big Al' + '</h3>');
+} else {
+    document.write('<h4>' + 'Welcome' + '</h4>');
+}
+
+if (userName == 'Baylor'){
+    document.write('<h3>' + 'Welcome Bear' + '</h3>');
+} else {
+    document.write('<h4>' + 'Welcome' + '</h4>');
+}
+
+if (userName == 'Villanova'){
+    document.write('<h3>' + 'Welcome Wildcat' + '</h3>');
+} else {
+    document.write;
+}
+
+if (userName == 'Arkansas'){
+    document.write;
+} else {
+    document.write;
+}
+
+if (userName == 'Oral Roberts'){
+    document.write('<h3>' + 'Welcome Golden Eagle' + '</h3>');
+} else {
+    document.write;
+}
+ 
+if (userName == 'Loyola'){
+    document.write('<h3>' + 'Welcome Wolf' + '</h3>');
+} else {
+    document.write('<h4>' + 'Welcome' + '</h4>');
+}
+
+if (userName == 'Oregon State'){
+    document.write('<h3>' + 'Welcome Beaver' + '</h3>');
+} else {
+    document.write;
+}
+
+if (userName == 'Syracuse'){
+    document.write('<h3>' + 'Welcome Otto' + '</h3>');
+} else {
+    document.write;
+}
+
+if (userName == 'Houston'){
+    document.write('<h3>' + 'Welcome Shasta ' + '</h3>');
+} else {
+    document.write;
+}
+
