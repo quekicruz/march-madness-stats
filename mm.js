28d95718995a9f970aa588588a1c0534e45c20b7
Quentin Cruz <napu0515@gmail.com>
2021-03-25 12:52:48 -0700

    marchmadness

--- mm.js
+++ mm.js
@@ -1,102 +1,98 @@
 let userName = prompt('Who is your Sweet 16 Team?');
 
+function getuserName() {
+    document.write(userName);
+    console.log (userName)
+    return userName}
 
-if (userName == 'Gonzaga'){
-    document.write('<h3>' + 'Welcome Bulldog' + '</h3>');
-}else 
-{
-        document.write;
+
+function getTeamName(){
+    if (userName == 'Gonzaga'){
+        document.write('<h2>' + 'Welcome Bulldog' + '</h2>');
     }
-
-if (userName == 'Creighton'){
-    document.write('<h3>' + 'Welcome BlueJay' + '</h3>');
-} else {
-    document.write;
-}
-
-if (userName == 'USC'){
-    document.write('<h3>' + 'Welcome Trojan' + '</h3>');
     
-} else {
+    else if (userName == 'Creighton'){
+        document.write('<h2>' + 'Welcome BlueJay' + '</h2>');
+    }
+    
+    else if (userName == 'USC'){
+        document.write('<h2>' + 'Welcome Trojan' + '</h2>');
+    }
+    
+    else if (userName == 'Oregon'){
+        document.write('<h2>' + 'Welcome Duck' + '</h2>');
+    }
+    
+    else if (userName == 'Michigan'){
+        document.write('<h2>' + 'Welcome Wolverine' + '</h2>');
+    }
+    
+    else if (userName == 'Florida State'){
+        document.write('<h2>' + 'Welcome Seminole' + '</h2>');
+    }
+    
+    else if (userName == 'UCLA'){
+        document.write('<h2>' + 'Welcome Bruin' + '</h2>');
+    }
+     
+    else if (userName == 'Alabama'){
+        document.write('<h3>' + 'Welcome Tide' + '</h2>');
+    }
+    
+    if (userName == 'Baylor'){
+        document.write('<h2>' + 'Welcome Bear' + '</h2>');
+    } else {
         document.write;
     }
+    
+    if (userName == 'Villanova'){
+        document.write('<h2>' + 'Welcome Wildcat' + '</h2>');
+    } else {
+        document.write;
+    }
+    
+    if (userName == 'Arkansas'){
+        document.write('<h2>' + 'Welcome Razorback ' + '</h2>')
+    } else {
+        document.write;
+    }
+    
+    if (userName == 'Oral Roberts'){
+        document.write('<h2>' + 'Welcome Golden Eagle' + '</h2>');
+    } else {
+        document.write;
+    }
+     
+    if (userName == 'Loyola'){
+        document.write('<h2>' + 'Welcome Rambler' + '</h2>');
+    
+    } else {
+        document.write;
+    }
+    
+    if (userName == 'Oregon State'){
+        document.write('<h2>' + 'Welcome Beaver' + '</h2>');
+    } else {
+        document.write;
+    }
+    
+    if (userName == 'Syracuse'){
+        document.write('<h2>' + 'Welcome Orange' + '</h2>');
+    } else {
+        document.write;
+    }
+    
+    if (userName == 'Houston'){
+        document.write('<h2>' + 'Welcome Cougar ' + '</h2>');
+    } else {
+        document.write;
+    } 
 
-if (userName == 'Oregon'){
-    document.write('<h3>' + 'Welcome Duck' + '</h3>');
-} else {
-    document.write;
+let moveForward = prompt('Do you want to your team to win?')
+    if(moveForward.toLowerCase() == 'yes'){
+        moveForward = prompt('Are you sure?')
+    if(moveForward.toLowerCase() == 'yes'){
+        moveForward = Image('https://live.staticflickr.com/3831/13372123064_9f7bede2d9_b.jpg')
+    };
+    }
 }
-
-if (userName == 'Michigan'){
-    document.write('<h3>' + 'Welcome Wolverine' + '</h3>');
-} else {
-    document.write;
-}
-
-if (userName == 'Florida State'){
-    document.write('<h3>' + 'Welcome Seminole' + '</h3>');
-} else {
-    document.write;
-}
-
-if (userName == 'UCLA'){
-    document.write('<h3>' + 'Welcome Bruin' + '</h3>');
-} else {
-    document.write;
-}
- 
-if (userName == 'Alabama'){
-    document.write('<h3>' + 'Welcome Tide' + '</h3>');
-} else {
-    document.write;
-}
-
-if (userName == 'Baylor'){
-    document.write('<h3>' + 'Welcome Bear' + '</h3>');
-} else {
-    document.write;
-}
-
-if (userName == 'Villanova'){
-    document.write('<h3>' + 'Welcome Wildcat' + '</h3>');
-} else {
-    document.write;
-}
-
-if (userName == 'Arkansas'){
-    document.write('<h3>' + 'Welcome Razorback ' + '</h3>')
-} else {
-    document.write;
-}
-
-if (userName == 'Oral Roberts'){
-    document.write('<h3>' + 'Welcome Golden Eagle' + '</h3>');
-} else {
-    document.write;
-}
- 
-if (userName == 'Loyola'){
-    document.write('<h3>' + 'Welcome Rambler' + '</h3>');
-
-} else {
-    document.write;
-}
-
-if (userName == 'Oregon State'){
-    document.write('<h3>' + 'Welcome Beaver' + '</h3>');
-} else {
-    document.write;
-}
-
-if (userName == 'Syracuse'){
-    document.write('<h3>' + 'Welcome Orange' + '</h3>');
-} else {
-    document.write;
-}
-
-if (userName == 'Houston'){
-    document.write('<h3>' + 'Welcome Cougar ' + '</h3>');
-} else {
-    document.write;
-}
-
---
+++ mm2.js
@@ -0,0 +1,7 @@
+let userName2 = prompt('Who is your Sweet 16 Team?');
+
+function getuserName2() {
+    document.write(userName2);
+    console.log (userName2)
+    return userName2}
+getuserName2()
